1c68fd02b41436b7f6e9664c8b7d192097390e92
alberto ariza <alberto0444@hotmail.com>
2021-02-18 15:52:30 -0500

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo");
+            Console.Write("Digite un numero :");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine($"el numero que usted digito es : {numero}");
+            Console.ReadKey();
+
+
+        }
+    }
+}
